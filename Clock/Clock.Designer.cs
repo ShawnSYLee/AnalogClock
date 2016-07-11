2c631a477a065ff0079da370e9be8f579a7bccab
Shawn Lee <leeshawn.os01@gmail.com>
2016-07-10 18:33:33 -0700

    Analog Clock initial checkins

---
+++ Clock/Clock.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Clock2
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clock));
+            this.ClockPanel = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ClockPanel
+            // 
+            this.ClockPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ClockPanel.BackgroundImage")));
+            this.ClockPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClockPanel.Location = new System.Drawing.Point(0, 0);
+            this.ClockPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.ClockPanel.Name = "ClockPanel";
+            this.ClockPanel.Size = new System.Drawing.Size(200, 200);
+            this.ClockPanel.TabIndex = 0;
+            this.ClockPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.ClockPanel_Paint);
+            this.ClockPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ClockPanel_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 200);
+            this.ControlBox = false;
+            this.Controls.Add(this.ClockPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Clock";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ClockPanel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Clock/Clock.cs
@@ -0,0 +1,114 @@
+﻿/*
+
+    Copyright © 2016, Shawn Lee
+
+*/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock2
+{
+    public partial class Clock : Form
+    {
+        private int centerX = 100;
+        private int centerY = 100;
+        private int radius = 100;
+
+        public Clock()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ClockPanel.Invalidate();
+        }
+
+        /// <summary>
+        /// Redraws Clock every second
+        /// </summary>
+        private void ClockPanel_Paint(object sender, PaintEventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            float x;
+            float y;
+
+            // Draw Seconds
+            ClockWorks(now.Second, radius, .90, out x, out y);
+
+            Pen redPen = new Pen(Color.Red, 2);
+            e.Graphics.DrawLine(redPen, centerX, centerY, x, y);
+            redPen.Dispose();
+            
+            // Draw Minutes
+            ClockWorks(now.Minute, radius, .85, out x, out y);
+
+            Pen blackPen = new Pen(Color.Black, 4);
+            e.Graphics.DrawLine(blackPen, centerX, centerY, x, y);
+            blackPen.Dispose();
+             
+            // Draw Hours
+            ClockWorks(now.Hour % 12 * 5 + now.Minute / 12, radius, .70, out x, out y);
+            
+            Pen blackPen2 = new Pen(Color.Black, 6);
+            e.Graphics.DrawLine(blackPen2, centerX, centerY, x, y);
+            blackPen2.Dispose();
+        }
+
+        /// <summary>
+        /// Calculates endpoint (x, y positions) of clock hands 
+        /// </summary>
+        /// <param name="time">seconds/minutes/hours</param>
+        /// <param name="radius">radius of clock</param>
+        /// <param name="shortenRatio">ratio to shorten clock hand</param>
+        /// <param name="xPosition">output X position of clock hand</param>
+        /// <param name="yPosition">output Y position of clock hand</param>
+        private void ClockWorks(int time, int radius, double shortenRatio, out float xPosition, out float yPosition)
+        {
+            double x = 0;
+            double y = 0;
+            int angle = 0;
+
+            if (time < 15)
+            {
+                angle = 90 - (time * 6);
+            }
+            else if (time < 30)
+            {
+                angle = 360 - (time % 15) * 6;
+            }
+            else if (time < 45)
+            {
+                angle = 270 - (time % 15) * 6;
+            }
+            else if (time < 60)
+            {
+                angle = 180 - (time % 15) * 6;
+            }
+
+            double ang = angle * (Math.PI / 180);
+
+            x = radius + (Math.Cos(ang) * radius * shortenRatio);
+            y = radius - (Math.Sin(ang) * radius * shortenRatio);
+
+            xPosition = Convert.ToSingle(x);
+            yPosition = Convert.ToSingle(y);
+        }
+
+        private void ClockPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(Cursor.Position.X, Cursor.Position.Y);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
